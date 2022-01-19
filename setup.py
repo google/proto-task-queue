9f70507d5461aec4322c02d949fb244ccd98af00
David Mandelberg <dseomn@google.com>
2022-01-19 18:54:49 -0500

    Require python >= 3.7
    https://github.com/google/proto-task-queue/runs/4875569422?check_suite_focus=true
    just failed for a PR that only adds comments, so I'm guessing the issue
    is unrelated to the PR. I don't think we're using 3.6 anymore, so I
    figured I'd drop support for it rather than figure out what's causing
    the failure.

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setuptools.setup(
         'proto_task_queue.task_pb2',
         'proto_task_queue.worker',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     setup_requires=[
         'pytest-runner>=4.2',
     ],
